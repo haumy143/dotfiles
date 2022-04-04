db3680b2527310873787ba7441f43eccfcd666f2
Clemens <66621176+haumy143@users.noreply.github.com>
2022-04-04 11:11:42 +0200

    added zsh-script

---
+++ Linux/install_ohmyzsh.sh
@@ -0,0 +1,9 @@
+!# /bin/bash
+
+sudo apt update
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+cp .zshrc ~/.zshrc
+cp pygmalion.zsh-theme ~/.oh-my-zsh/themes/pygmalion.zsh-theme
+source .zshrc
