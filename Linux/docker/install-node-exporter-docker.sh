267e2490373e3163574fffa063c5f14977bd7310
Clemens <66621176+haumy143@users.noreply.github.com>
2022-05-09 22:27:06 +0200

    Update install-node-exporter-docker.sh

--- Linux/docker/install-node-exporter-docker.sh
+++ Linux/docker/install-node-exporter-docker.sh
@@ -3,16 +3,19 @@ IP=$(hostname  -I | cut -f1 -d' ')
 
 if ! command -v docker --version &> /dev/null
 then
-    echo "docker is not installed"
-    exit
-else
-    docker run -d \
-    --net="host" \
-    --pid="host" \
-    -v "/:/host:ro,rslave" \
-    quay.io/prometheus/node-exporter:latest \
-    --path.rootfs=/host
-    echo "node-exporter is installed"
-    echo "add $IP:9100 to Prometheus targets(/etc/prometheus/prometheus.yaml)"
-    exit
+    read -p "docker is not installed! Do you want to apt install Docker.io now?(y/n)" -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+    apt-get install docker.io -y
+    fi
 fi
+
+docker run -d \
+--net="host" \
+--pid="host" \
+-v "/:/host:ro,rslave" \
+quay.io/prometheus/node-exporter:latest \
+--path.rootfs=/host
+echo "node-exporter is now installed"
+echo "####################### add $IP:9100 to Prometheus targets(/etc/prometheus/prometheus.yaml) #######################"
+exit
