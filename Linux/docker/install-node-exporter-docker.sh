1c952dcc4747915c2125339b824f72331312070f
Clemens <66621176+haumy143@users.noreply.github.com>
2022-05-09 23:11:38 +0200

    Update install-node-exporter-docker.sh

--- Linux/docker/install-node-exporter-docker.sh
+++ Linux/docker/install-node-exporter-docker.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-IP=$(hostname  -I | cut -f1 -d' ') 
+IP=$(hostname  -I | cut -f1 -d' ')
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
 
 if ! command -v docker --version &> /dev/null
 then
-    read -p "docker is not installed! Do you want to apt install Docker.io now?(y/n)" -n 1 -r
+    read -p "docker is not installed! Do you want to apt install Docker.io now?(y/n):" -n 1 -r
     if [[ $REPLY =~ ^[Yy]$ ]]
-    then
-    apt-get install docker.io -y
+        then
+        apt-get update
+        apt-get install docker.io -y
+    else
+        exit
     fi
 fi
 
@@ -14,8 +20,10 @@ docker run -d \
 --net="host" \
 --pid="host" \
 -v "/:/host:ro,rslave" \
+--restart always \
 quay.io/prometheus/node-exporter:latest \
 --path.rootfs=/host
-echo "node-exporter is now installed"
-echo "####################### add $IP:9100 to Prometheus targets(/etc/prometheus/prometheus.yaml) #######################"
+
+echo -e "${GREEN}node-exporter container is now installed${NC}"
+echo -e "Add ${YELLOW}$IP:9100${NC} to Prometheus targets(/etc/prometheus/prometheus.yaml)"
 exit
